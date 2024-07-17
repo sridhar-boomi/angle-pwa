076697738e987a39f806df7147b05ab2dbdc107e
Sridhar Boomi <Sridhar.Boomi@allegion.com>
2024-07-17 20:34:07 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angle-pwa';
+  latestBeta: number = 0;
+  latestGamma: number = 0;
+  tiltAngle: number = 0;
+  ngOnInit(): void {
+    if (window.DeviceOrientationEvent) {
+      window.addEventListener('deviceorientation', this.handleOrientation, true);
+    } else {
+      alert("DeviceOrientationEvent is not supported on this device.");
+      //console.log("DeviceOrientationEvent is not supported on this device.");
+    }
+  }
+
+  handleOrientation = (event: DeviceOrientationEvent) => {
+    this.latestBeta = event.beta ?? 0;
+    this.latestGamma = event.gamma ?? 0;
+    this.tiltAngle = Math.sqrt((this.latestBeta ? this.latestBeta * this.latestBeta : 0) + (this.latestGamma ? this.latestGamma * this.latestGamma : 0));
+  }
+
+  // calculateTiltAngle(){
+  //   alert("calculateTiltAngle");
+  //   this.tiltAngle = Math.sqrt((this.latestBeta ? this.latestBeta * this.latestBeta : 0) + (this.latestGamma ? this.latestGamma * this.latestGamma : 0));
+  // }
 }
